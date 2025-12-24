3508e75cac7c33a146b26739d66c74dec1a6836c
Ciaran O'Brien <ciaranobrien@Ciarans-MacBook-Pro.local>
2025-12-24 15:09:53 +0000

    :rocket: Nl-cli-dev project and 2x Firebase configs,dv/pd

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,15 +1,66 @@
 # IAM Groups and Permissions
 
-# Admin group
-resource "google_project_iam_member" "admin_group" {
-  project = var.project_id
-  role    = "roles/storage.admin"
-  member  = "user:ciaranobrienmusic@gmail.com"
+# DEV PROJECT PERMISSIONS
+# Project owner - Dev
+resource "google_project_iam_member" "project_owner_dev" {
+  project = var.project_id_dev
+  role    = "roles/owner"
+  member  = "user:${var.owner_email}"
 }
 
-# ML Engineers group - Editor access at project level
-resource "google_project_iam_member" "ml_engineers_editor" {
-  project = var.project_id
+# ML Engineers group - Editor access - Dev
+resource "google_project_iam_member" "ml_engineers_editor_dev" {
+  project = var.project_id_dev
   role    = "roles/editor"
-  member  = "group:spicy-lemonage-nl-cli-ml-engineers@googlegroups.com"
+  member  = "group:${var.ml_group_email}"
+}
+
+# Firestore/Datastore permissions for owner - Dev
+resource "google_project_iam_member" "owner_datastore_dev" {
+  project = var.project_id_dev
+  role    = "roles/datastore.owner"
+  member  = "user:${var.owner_email}"
+}
+
+# Firestore/Datastore permissions for ML engineers - Dev
+resource "google_project_iam_member" "ml_engineers_datastore_dev" {
+  project = var.project_id_dev
+  role    = "roles/datastore.user"
+  member  = "group:${var.ml_group_email}"
+}
+
+# PROD PROJECT PERMISSIONS
+# Project owner - Prod
+resource "google_project_iam_member" "project_owner_prod" {
+  project = var.project_id_prod
+  role    = "roles/owner"
+  member  = "user:${var.owner_email}"
+}
+
+# Storage admin access - Prod
+resource "google_project_iam_member" "admin_group_prod" {
+  project = var.project_id_prod
+  role    = "roles/storage.admin"
+  member  = "user:${var.owner_email}"
+}
+
+# ML Engineers group - Editor access - Prod
+resource "google_project_iam_member" "ml_engineers_editor_prod" {
+  project = var.project_id_prod
+  role    = "roles/editor"
+  member  = "group:${var.ml_group_email}"
+}
+
+# Firestore/Datastore permissions for owner - Prod
+resource "google_project_iam_member" "owner_datastore_prod" {
+  project = var.project_id_prod
+  role    = "roles/datastore.owner"
+  member  = "user:${var.owner_email}"
+}
+
+# Firestore/Datastore permissions for ML engineers - Prod
+resource "google_project_iam_member" "ml_engineers_datastore_prod" {
+  project = var.project_id_prod
+  role    = "roles/datastore.user"
+  member  = "group:${var.ml_group_email}"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,11 @@
-variable "project_id" {
-  description = "GCP project ID"
+variable "project_id_dev" {
+  description = "GCP project ID for dev environment"
+  type        = string
+  default     = "nl-cli-dev"
+}
+
+variable "project_id_prod" {
+  description = "GCP project ID for prod environment"
   type        = string
   default     = "nl-cli"
 }
@@ -32,4 +38,16 @@ variable "enable_versioning" {
   description = "Enable object versioning on the bucket"
   type        = bool
   default     = true
+}
+
+variable "owner_email" {
+  description = "Email address of the project owner"
+  type        = string
+  default     = "ciaranobrienmusic@gmail.com"
+}
+
+variable "ml_group_email" {
+  description = "Google Group email for ML engineers"
+  type        = string
+  default     = "spicy-lemonage-nl-cli-ml-engineers@googlegroups.com"
 }
